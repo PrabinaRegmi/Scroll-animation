c873b1bac1e07e42ab15f77223dc57777b52fa35
Prabina Regmi <git@prabina.us>
2023-04-21 13:09:12 -0600

    changed translateX value from 200% to 400%

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ function checkbox() {
     const boxTop = box.getBoundingClientRect().top;
     if (boxTop < triggerBottom) {
       box.classList.add("show");
+    } else {
+      box.classList.remove("show");
     }
   });
 }
